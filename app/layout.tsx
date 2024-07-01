3cfbbdde8e78aa4e0093e1e912654ddfc3323e9e
Filipe Silva <fgalego@gmail.com>
2024-07-01 15:21:36 +0100

    img warning, and layout hydration suppress

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     // <ClerkProvider>
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={noto.className}>
         <ThemeProvider
           attribute="class"
--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -9,7 +9,15 @@ export function Logo() {
       className="min-h-20 h-20 flex items-center px-6 border-b cursor-pointer gap-2"
       onClick={() => router.push('/')}
     >
-      <Image src="/lrb_black.svg" alt="Logo" width={70} height={70} priority />
+      <div className="relative w-16 h-16">
+        <Image
+          src="/lrb_black.svg"
+          alt="Logo"
+          fill
+          style={{objectFit: 'contain'}}
+          priority
+        />
+      </div>
       <h1 className="font-bold text-xl ml-4 ">Arvoredo</h1>
     </div>
   );
