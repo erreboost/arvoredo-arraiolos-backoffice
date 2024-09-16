e92a450a871121e858045e0f823f59b4c55b96bd
btrzdev <btrz.dev@gmail.com>
2024-09-16 12:45:07 +0100

    fix link initial page

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -1,24 +1,21 @@
-'use client'
-import Image from 'next/image'
-import { useRouter } from 'next/navigation'
+"use client";
+import Image from "next/image";
+import Link from "next/link";
 export function Logo() {
-  const router = useRouter()
-
   return (
-    <div
-      className="flex h-20 min-h-20 cursor-pointer items-center gap-2 border-b px-6"
-      onClick={() => router.push('/')}
-    >
+    <div className="flex h-20 min-h-20 items-center gap-2 border-b px-6">
       <div className="relative h-16 w-16">
-        <Image
-          src="/lrb_black.svg"
-          alt="Logo"
-          fill
-          style={{ objectFit: 'contain' }}
-          priority
-        />
+        <Link href={"https://lrb.pt/"}>
+          <Image
+            src="/lrb_black.svg"
+            alt="Logo"
+            fill
+            style={{ objectFit: "contain" }}
+            priority
+          />
+        </Link>
       </div>
       <h1 className="ml-4 text-xl font-bold">Arvoredo</h1>
     </div>
-  )
+  );
 }
