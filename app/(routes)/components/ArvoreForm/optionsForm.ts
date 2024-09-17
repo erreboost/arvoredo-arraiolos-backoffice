a46d27e82684bf4bfdbd938b0a0b5aecf2ce4fd9
btrzdev <btrz.dev@gmail.com>
2024-09-17 18:47:58 +0100

    fix: form fix changes

---
+++ app/(routes)/components/ArvoreForm/optionsForm.ts
@@ -0,0 +1,123 @@
+export const OPTIONS_LOCALIZACAO = [
+  "Via",
+  "Passeio",
+  "Jardim",
+  "Infraestruturas",
+];
+export const OPTIONS_ESTADO_FIT = [
+  "Árvore viva, ausência de sintomas ou danos.",
+  "Árvore viva, presença de sintomas ou sinais.",
+  "Árvore morta.",
+];
+export const OPTIONS_ESTADO_CAL = [
+  "Caldeira suficiente e em bom estado.",
+  "Caldeira insuficiente.",
+  "Inexistente",
+];
+export const OPTIONS_AVA_RISCO = ["Sem Risco", "Com Risco"];
+export const OPTIONS_RAZ_CLASSIFICA = [
+  "Não Aplicável",
+  "Árvore de interesse Público",
+];
+export const OPTIONS_AGEN_BIOTICOS = [
+  "Sem Sinais.",
+  "Presença de insetos.",
+  "Presença de fungos.",
+  "Presença de insetos e fungos.",
+];
+export const OPTIONS_ORGAOS_AFETADOS = [
+  "Sem orgãos afetados.",
+  "Troncos.",
+  "Ramos",
+  "Raiz.",
+  "Folhas.",
+  "Vários orgãos afetados.",
+];
+export const OPTIONS_GRAU_DESFOLHA = [
+  "0 - 10%",
+  "11 - 25%",
+  "26 - 60%",
+  "61 - 90%",
+  ">90%",
+];
+export const OPTIONS_SINTOMAS_SINAIS_DESFOLHADORES = [
+  "Sem sintomas ou sinais.",
+  "Folhas com orifícios, enroladas ou roídas.",
+  "Ninhos de proteção.",
+];
+export const OPTIONS_GRAU_DESCOLORACAO_COPA = [
+  "0 - 10%",
+  "11 - 25%",
+  "26 - 60%",
+  "61 - 90%",
+  ">90%",
+];
+export const OPTIONS_DEFORMACAO_DOS_TECIDOS = [
+  "Sem deformações.",
+  "Galhas.",
+  "Tumores/cancros.",
+  "Engrossamentos.",
+  "Distorções.",
+  "Vassoura de bruxa.",
+];
+export const OPTIONS_ALTERACAO_DA_ESTRUTURA = [
+  "Sem deformações.",
+  "Casca solta.",
+  "Casca com fissuras/fendilhada.",
+  "Deformações do tronco.",
+  "Folhas enroladas/encarquilhadas.",
+  "Microfilia das folhas.",
+];
+export const OPTIONS_SUPRESSAO_PARCIAL_ORGAOS = [
+  "Sem lesões.",
+  "Esqueletizados (folhas).",
+  "Roídos.",
+  "Cortados.",
+  "Só com nervura principal (folhas).",
+];
+export const OPTIONS_ORIFICIOS_PERFURACOES = [
+  "Sem orifícios.",
+  "Circulares .",
+  "Claramente ovais.",
+  "Ovais de contornos assimétricos.",
+  "Em forma de “D”.",
+];
+export const OPTIONS_GALERIA = [
+  "Sem galerias.",
+  "Irregulares.",
+  "Lineares.",
+  "Em estrela.",
+  "Com curvaturas acentuadas.",
+];
+export const OPTIONS_NECROSES = [
+  "Sem necroses.",
+  "Uniformes.",
+  "Pontos.",
+  "Manchas.",
+  "Pontos e manchas.",
+];
+export const OPTIONS_SERRIM = ["Sem serrim.", "Claro.", "Escuro."];
+export const OPTIONS_EXUDACAO = [
+  "Sem exsudação.",
+  "Em gotas.",
+  "Em pequenas placas.",
+  "Em massa.",
+];
+export const OPTIONS_NOVELOS_FIBRA = ["Sem novelos.", "Presença de novelos."];
+export const OPTIONS_FORMA_CALDEIRA = [
+  "Quadrada",
+  "Redonda",
+  "Outra",
+  "Inexistente",
+];
+
+export const OPTIONS_IDADE_APROV_V2 = [
+  "< 5 anos",
+  "5 -10 anos",
+  "10 -20 anos",
+  "20 -30 anos",
+  "30 -40 anos",
+  "40 -50 anos",
+  "50 -60 anos",
+  "> 60 anos",
+];
